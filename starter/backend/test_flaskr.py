63d9cb98d42d1b3211eece09a08d657675cd4c3e
Tununung <37055373+Tununung@users.noreply.github.com>
2021-01-26 17:18:22 +0800

    20210126

--- starter/backend/test_flaskr.py
+++ starter/backend/test_flaskr.py
@@ -29,10 +29,136 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    """ Test endpoint GET /categories"""
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_categories'])
+        self.assertTrue(len(data['categories']))
+
+    """ Test endpoint GET /questions"""
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_404_get_questions_invalid_page(self):
+        res = self.client().get('questions/?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    """ Test endpoint DELETE /questions"""
+    def test_delete_question(self):
+        question_id = 2
+
+        res = self.client().delete('/questions/{}'.format(question_id))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], question_id)
+
+    def test_422_request_delete_nonexisting_question(self):
+        question_id = 1000
+        res = self.client().delete('/questions/{}'.format(question_id))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
+
+    """ Test endpoint POST /questions"""
+    def test_create_question(self):
+        res = self.client().post('/questions', json={
+                "question": "Why did the chicken cross the road?",
+                "answer": "To get to the other side.",
+                "difficulty": 1,
+                "category": "4"
+            })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+
+    def test_422_request_create_question_empty_payload(self):
+        res = self.client().post('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
+
+    """ Test endpoint POST /questions/search"""
+    def test_search_questions(self):
+        res = self.client().post('/questions/search', json={"searchTerm": "?"})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_search_questions_empty_payload(self):
+        res = self.client().post('/questions/search')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
+
+    """ Test endpoint POST /questions/<int:category_id>/questions"""
+    def test_get_questions_by_category(self):
+        category_id = 1
+        res = self.client().get('/categories/{}/questions'.format(category_id))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['current_category'])
+
+    def test_404_get_questions_by_category_invalid_category_id(self):
+        category_id = 1000
+        res = self.client().get('/categories/{}/questions'.format(category_id))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    """ Test endpoint POST /quizzes"""
+    def test_get_quiz_questions(self):
+        category_id = 1
+        res = self.client().post('/quizzes', json={
+                "previous_questions": [],
+                "quiz_category": {"type": "Test", "id": category_id}
+            })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+        self.assertTrue(data['previous_questions'])
+
+    def test_422_get_quiz_questions_empty_payload(self):
+        res = self.client().post('/quizzes')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
 
 
 # Make the tests conveniently executable
